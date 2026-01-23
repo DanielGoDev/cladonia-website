748b04e10e6cfa4d22ea7ad6c679a6722f9ee46e
Daniel González <danielgo.softdev@gmail.com>
2026-01-22 22:54:16 -0500

    Actualización de la section Map e incorporción de iconos

--- app/experiencies/sections/Map.js
+++ app/experiencies/sections/Map.js
@@ -2,21 +2,74 @@ import Image from "next/image";
 
 export default function Map() {
     return (
-        <section className="relative w-full min-h-screen bg-[#EEE] flex flex-col text-black justify-between">
-            <h2 className="text-3xl font-semibold text-center mb-2 px-4">
-            Mapa del Recorrido
-            </h2>
-            <div className="relative max-w-300 w-md h-8 bg-amber-400 flex mb-6" >
-                <nav className="relative max-w-300 bg-amber-400 flex"></nav>
+        <section className="relative w-full min-h-screen bg-[#EEE] flex flex-col text-black justify-between items-center">
+            <div className="mt-36 ">
+                <h2 className="text-4xl font-semibold text-center mb-2 px-4">
+                Mapa del Recorrido
+                </h2>
+                <div className="relative w-full md:w-7xl mx-auto flex flex-col md:flex-row items-center justify-between gap-2 md:gap-0 lg:gap-2 px-6 py-4 mb-6">
+                    <div className="flex flex-col md:flex-row items-center justify-between gap-4 md:gap-0 w-full">
+                        <div className="relative w-auto flex flex-row items-center justify-center rounded-full border-2 border-black px-3 py-1">
+                             <Image
+                             src="/icons/mountains.svg"
+                             alt="books icon"
+                             width={32}
+                             height={32}
+                             className="w-9.5 h-9.5 object-contain mx-2"
+                             />
+                             <p className="relative text-center text-balance font-bold">Meditación Ancestral</p>
+                        </div>
+                        <div className="relative w-auto flex flex-row items-center justify-center rounded-full border-2 border-black px-3 py-1">
+                             <Image
+                             src="/icons/prismatics.svg"
+                             alt="books icon"
+                             width={32}
+                             height={32}
+                             className="w-8.5 h-8.5 object-contain mx-2"
+                             />
+                             <p className="relative text-center text-balance font-bold">Ojo de la Montaña</p>
+                        </div>
+                        <div className="relative w-auto flex flex-row items-center justify-center rounded-full border-2 border-black px-3 py-1">
+                             <Image
+                             src="/icons/bird.svg"
+                             alt="books icon"
+                             width={32}
+                             height={32}
+                             className="w-8 h-8 object-contain mx-2"
+                             />
+                             <p className="relative text-center text-balance font-bold">Avistamiento de aves</p>
+                        </div>
+                        <div className="relative w-auto flex flex-row items-center justify-center rounded-full border-2 border-black px-3 py-1">
+                             <Image
+                             src="/icons/flower.svg"
+                             alt="books icon"
+                             width={32}
+                             height={32}
+                             className="w-8 h-8 object-contain mx-2"
+                             />
+                             <p className="relative text-center text-balance font-bold">Conexión Natural</p>
+                        </div>
+                        <div className="relative w-auto flex flex-row items-center justify-center rounded-full border-2 border-black px-3 py-1">
+                             <Image
+                             src="/icons/books.svg"
+                             alt="books icon"
+                             width={32}
+                             height={32}
+                             className="w-8 h-8 object-contain mx-2"
+                             />
+                             <p className="relative text-center text-balance font-bold">Memoria Viva</p>
+                        </div>
+                        
+                    </div>
+                </div>
             </div>
-
             
-            <div className=" relative bottom-0 max-w-360 h-1/2 flex justify-center w-full md:w-3/4 lg:w-2/3 xl:w-1/2 aspect-video ">
+            <div className="relative w-full md:aspect-video xl:w-340 aspect-video">
                 <Image
                     src="/images/map.png"
                     alt="Mapa del Parque Natural Nacional Chingaza"
                     fill
-                    className=" object-cover object-bottom"
+                    className="object-contain"
                 />
             </div>
         </section>
