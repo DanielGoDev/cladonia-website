d951b9b0cf6aa2b5e6565dbab8a191f0a53fec04
Daniel González <danielgo.softdev@gmail.com>
2026-02-03 12:36:59 -0500

    Cambios locales en footer y contacto (PC escritorio)

--- components/ui/Footer.js
+++ components/ui/Footer.js
@@ -2,7 +2,7 @@
 export default function Footer() {
   return (
     <footer className="bg-black text-white py-10 text-center bottom-0">
-      <p>© 2026 Cladonia S.A.S <br /> Registro Nacional de Turismo <br /> n.256419 </p>
+      <p>© 2026 ALMONTI by Cladonia S.A.S <br /> Registro Nacional de Turismo <br /> n.256419 </p>
 
     </footer>
   );
--- sections/Contact.js
+++ sections/Contact.js
@@ -1,20 +1,54 @@
+import Image from "next/image";
+
 export default function Contact() {
     return (
         <section className="relative w-screen min-h-160 bg-black text-white py-20">  
             <div className="max-w-7xl mx-auto px-6">
                 <h2 className="text-4xl font-bold text-center mb-24">Contacto</h2>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-12 max-w-sm md:max-w-none mx-auto">
-                    <div className="bg-gray-800 p-4 md:p-6 rounded-lg text-center aspect-square flex flex-col justify-center">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 md:gap-12 max-w-sm md:max-w-none mx-auto">
+                    <div className="bg-black p-4 md:p-6 rounded-2xl text-center aspect-square flex flex-col justify-center shadow-white shadow-lg">
+                        <Image
+                            src="/icons/whatsappLogo.svg"
+                            alt="Icono de teléfono"
+                            width={48}
+                            height={48}
+                            className="mx-auto mb-4"
+                        />
                         <h3 className="text-lg md:text-xl font-bold mb-2 md:mb-4">Teléfono</h3>
                         <p className="text-gray-300 text-sm md:text-base">+57 300 123 4567</p>
                     </div>
-                    <div className="bg-gray-800 p-4 md:p-6 rounded-lg text-center aspect-square flex flex-col justify-center">
+                    <div className="bg-black p-4 md:p-6 rounded-2xl text-center aspect-square flex flex-col justify-center shadow-white shadow-lg">
+                        <Image
+                            src="/icons/facebook.svg"
+                            alt="Icono de Facebook"
+                            width={48}
+                            height={48}
+                            className="mx-auto mb-4"
+                        />
+                        <h3 className="text-lg md:text-xl font-bold mb-2 md:mb-4">Facebook</h3>
+                        <p className="text-gray-300 text-sm md:text-base">@CladoniaTurismo</p>
+                    </div>
+                    <div className="bg-black p-4 md:p-6 rounded-2xl text-center aspect-square flex flex-col justify-center shadow-white shadow-lg">
+                        <Image
+                            src="/icons/instagramLogo.svg"
+                            alt="Icono de Instagram"
+                            width={48}
+                            height={48}
+                            className="mx-auto mb-4"
+                        />
+                        <h3 className="text-lg md:text-xl font-bold mb-2 md:mb-4">Instagram</h3>
+                        <p className="text-gray-300 text-sm md:text-base">@cladonia_eco</p>
+                    </div>
+                    <div className="bg-black p-4 md:p-6 rounded-2xl text-center aspect-square flex flex-col justify-center shadow-white shadow-lg">
+                        <Image
+                            src="/icons/mail.svg"
+                            alt="Icono de email"
+                            width={48}
+                            height={48}
+                            className="mx-auto mb-4"
+                        />
                         <h3 className="text-lg md:text-xl font-bold mb-2 md:mb-4">Email</h3>
                         <p className="text-gray-300 text-sm md:text-base">info@cladonia.com</p>
-                    </div>
-                    <div className="bg-gray-800 p-4 md:p-6 rounded-lg text-center aspect-square flex flex-col justify-center">
-                        <h3 className="text-lg md:text-xl font-bold mb-2 md:mb-4">Ubicación</h3>
-                        <p className="text-gray-300 text-sm md:text-base">Bogotá, Colombia</p>
                     </div>
                 </div>
             </div>
